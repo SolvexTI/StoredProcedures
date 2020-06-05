f9497ce83c9514a9f44d7b2a064d0a9a80c33df9
adelpino <alejandro.ignacio.dpa@gmail.com>
2020-06-04 20:20:09 -0400

    Creacion Paquete Cliente
    Se crea paquete cliente con tipo y metodos respectivos

--- Consultas/Procedimientos_Package.sql
+++ Consultas/Procedimientos_Package.sql
@@ -14,6 +14,8 @@ CREATE OR REPLACE PACKAGE pkg_profesional AS
     dv               VARCHAR2(1)
   );
 
+  TYPE tb_profesional IS TABLE OF tp_profesional;
+
   FUNCTION fn_obtener_profesional(p_id_usuario usuario.id_usuario%TYPE) RETURN tp_profesional;
   PROCEDURE pr_insertar_profesional(
     p_username in usuario.username%TYPE,
@@ -27,7 +29,7 @@ CREATE OR REPLACE PACKAGE pkg_profesional AS
     p_dv in profesional.dv%TYPE,
     p_profesional out tp_profesional
   );
-
+  PROCEDURE pr_eliminar_profesional(p_id_usuario in usuario.id_usuario%TYPE);
 
 END pkg_profesional;
 /
@@ -67,5 +69,89 @@ CREATE OR REPLACE PACKAGE BODY pkg_profesional AS
     p_profesional := fn_obtener_profesional(id_usuario_seq.CURRVAL);
   END;
 
+  PROCEDURE pr_eliminar_profesional(p_id_usuario in usuario.id_usuario%TYPE) AS
+  BEGIN
+    DELETE FROM profesional WHERE id_usuario = p_id_usuario;
+    DELETE FROM usuario WHERE id_usuario = p_id_usuario;
+  END;
 END pkg_profesional;
+/
+
+
+CREATE OR REPLACE PACKAGE pkg_cliente AS
+
+  TYPE tp_cliente IS RECORD (
+    id_usuario       NUMBER(38),
+    username         VARCHAR2(80),
+    password         VARCHAR2(40),
+    telefono         VARCHAR2(12),
+    correo           VARCHAR2(80),
+    id_cliente       NUMBER(38),
+    nombre           VARCHAR2(100),
+    direccion        VARCHAR2(120),
+    rubro            VARCHAR2(100),
+    id_profesional   NUMBER(38)
+  );
+
+  TYPE tb_cliente IS TABLE OF tp_cliente;
+
+  FUNCTION fn_obtener_cliente(p_id_usuario usuario.id_usuario%TYPE) RETURN tp_cliente;
+  PROCEDURE pr_insertar_cliente(
+    p_username in usuario.username%TYPE,
+    p_password in usuario.password%TYPE,
+    p_telefono in usuario.telefono%TYPE,
+    p_correo in usuario.correo%TYPE,
+    p_nombre in cliente.nombre%TYPE,
+    p_direccion in cliente.direccion%TYPE,
+    p_rubro in cliente.rubro%TYPE,
+    p_id_profesional in cliente.id_profesional%TYPE,
+    p_cliente out tp_cliente
+  );
+  PROCEDURE pr_eliminar_cliente(p_id_usuario in usuario.id_usuario%TYPE);
+END pkg_cliente;
+/
+
+CREATE OR REPLACE PACKAGE BODY pkg_cliente AS
+  FUNCTION fn_obtener_cliente (p_id_usuario usuario.id_usuario%TYPE) RETURN tp_cliente
+  AS
+    r_cliente tp_cliente;
+  BEGIN
+    SELECT id_usuario,username,password,telefono,correo,id_cliente,nombre,direccion,rubro,id_profesional
+    INTO r_cliente.id_usuario,r_cliente.username,r_cliente.password,r_cliente.telefono,r_cliente.correo,r_cliente.id_cliente,r_cliente.nombre,r_cliente.direccion,r_cliente.rubro,r_cliente.id_profesional
+    FROM cliente JOIN usuario USING(id_usuario)
+    WHERE id_usuario = p_id_usuario;
+
+    RETURN r_cliente;
+  END;
+
+  PROCEDURE pr_insertar_cliente (
+    p_username in usuario.username%TYPE,
+    p_password in usuario.password%TYPE,
+    p_telefono in usuario.telefono%TYPE,
+    p_correo in usuario.correo%TYPE,
+    p_nombre in cliente.nombre%TYPE,
+    p_direccion in cliente.direccion%TYPE,
+    p_rubro in cliente.rubro%TYPE,
+    p_id_profesional in cliente.id_profesional%TYPE,
+    p_cliente out tp_cliente)
+  AS
+  BEGIN
+    INSERT INTO usuario (id_usuario,username, password, telefono, correo, id_rol)
+    VALUES(id_usuario_seq.NEXTVAL, p_username, p_password, p_telefono, p_correo, 3);
+
+    INSERT INTO cliente (id_usuario,id_cliente,nombre,direccion,rubro,id_profesional)
+    VALUES(id_usuario_seq.CURRVAL,id_cliente_seq.NEXTVAL,p_nombre,p_direccion,p_rubro,p_id_profesional);
+
+    p_cliente := fn_obtener_cliente(id_usuario_seq.CURRVAL);
+  END;
+
+
+  PROCEDURE pr_eliminar_cliente(p_id_usuario in usuario.id_usuario%TYPE)
+  AS
+  BEGIN
+    DELETE cliente WHERE id_usuario = p_id_usuario;
+    DELETE usuario WHERE id_usuario = p_id_usuario;
+  END;
+
+END;
 /

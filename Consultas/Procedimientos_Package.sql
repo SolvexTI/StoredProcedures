23901f3a9beda30668fa838f83da4d5f2f8849d0
Alvaro San Martin <alvaro.sanmartinh@gmail.com>
2020-07-14 19:54:23 -0400

    fix

--- Consultas/Procedimientos_Package.sql
+++ Consultas/Procedimientos_Package.sql
@@ -472,7 +472,7 @@ CREATE OR REPLACE PACKAGE BODY pkg_actividad AS
   END;
 
   PROCEDURE pr_modificar_actividad(
-    p_id_actividad in actividad.id_actividad%TYPE
+    p_id_actividad in actividad.id_actividad%TYPE,
     p_nombre in actividad.nombre%type,
     p_descripcion in actividad.descripcion%type,
     p_estado in actividad.estado%type,
